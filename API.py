09cfd60769e476d608b5d89f93e67d4b42dd47ec
robinson-85 <robinpaulista@yahoo.com.br>
2020-08-03 18:47:54 -0300

    Aprendendo sobre API

---
+++ API.py
@@ -0,0 +1,16 @@
+Cliente (Client)
+Garçom (pedidos, leva seus pedidos para cozinha)(API)
+Cozinha (Server)
+
+Aplication Programming Interface (API) é 
+basicamente um conjunto de rotinas e padrões 
+estabelecidos por uma aplicação, para que
+outras aplicações possam utilizar as 
+funcionalidades dessa aplicação.
+
+API é:
+
+-Responsável por estabelecer comunicação
+entre diferentes serviços;
+-Meio de campo entre as tecnologias;
+-Intermediadores para trocas de informações.
