ce0063c785792309162309c3ac77b3436ef10e4b
robinson-85 <robinpaulista@yahoo.com.br>
2020-08-03 22:40:49 -0300

    Aprendendo sobre Rest

---
+++ Rest.js
@@ -0,0 +1,15 @@
+Rest é um acrônimo para Representational
+State Transfer (Transferência de Estado 
+Representativo);
+
+-É feita a transferência de dados de uma maneira 
+simbólica, figurativa, representativa, de maneira
+didática;
+
+-A transferência de dados, geralmente, são feitas
+usando o protocolo HTTP;
+
+-O REST, delimita algumas obriagações nessas 
+transferências de dados;
+
+-Resources é então, uma entidade, um objeto.
