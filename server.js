4f34ea802671e9cee8321b8a33918a18418af829
robinson-85 <robinpaulista@yahoo.com.br>
2020-08-18 15:06:56 -0300

    Alterações 2

--- server.js
+++ server.js
@@ -8,10 +8,42 @@ app.get("/clients", function(req, res) {
     res.json(data);
 });
 
-app.get("/clients:id", function(req, res) {});
-app.post("/clients", function(req, res) {});
-app.put("/clients:id", function(req, res) {});
-app.delete("/clients:id", function(req, res) {});
+app.get("/clients/:id", function(req, res) {
+    const { id } = req.params;
+    const client = data.find(cli => cli.id = id);
+
+    if (!client) return res.status(204).json();
+
+    res.json(client);
+});
+
+app.post("/clients", function(req, res) {
+    const { name, email } = req.body;
+
+    // salvar
+    
+    res.json({ name, email});
+});
+
+app.put("/clients:id", function(req, res) {
+    const { id } = req.params;
+    const client = data.find(cli => cli.id = id);
+
+    if (!client) return res.status(204).json();
+
+    const { name } = req.body;
+
+    client.name = name;
+
+    res.json(client);
+});
+
+app.delete("/clients/:id", function(req, res) {
+    const { id } = req.params;
+    const clientsFiltered = data.filter(client => client.id != id);
+
+    res.json(clientsFiltered);
+});
 
 app.listen(3000, function(){
     console.log("Server is running");
